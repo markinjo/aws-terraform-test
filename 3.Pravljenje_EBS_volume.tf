0fae8dc87a9c3a025516b48a609cb65d8dc1e26d
markinjo19 <marko.petrovic96@gmail.com>
2023-07-23 08:01:40 +0200

    Dodavanje EBS voluma EC2 instanci

--- 3.Pravljenje_EBS_volume.tf
+++ 3.Pravljenje_EBS_volume.tf
@@ -1,10 +1,17 @@
 #Kreiranje EBS voluma
 
 resource "aws_ebs_volume" "example_volume" {
-  availability_zone = "eu-central-1a" # Izaberite željenu AWS zonu
-  size              = 15           # Velicina volumena u GB
-  type              = "gp2"        # Tip volumena (General Purpose SSD)
+  availability_zone = "eu-central-1b" # Izaberite željenu AWS zonu
+  size              = 15              # Velicina volumena u GB
+  type              = "gp2"           # Tip volumena (General Purpose SSD)
   tags = {
     Name = "Terr-a EBS Volume"
   }
+}
+
+#Dodavanje EBS voluma EC2 instanci 
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example_volume.id
+  instance_id = aws_instance.myec2vm.id
 }
